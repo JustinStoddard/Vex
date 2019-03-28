52a2d132a33baed90ff7ff7e53f0f7e176a321c5
Justin Stoddard <justinst@USA-IT-B8BG8WP.corp.unicity.net>
2019-03-28 14:34:24 -0600

    making sure commits work

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
+  
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
